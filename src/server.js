5df3c574ec450d439c9e22f3d8bf87c9cf946f3f
Kanak Latwal <kanaklatwal@gmail.com>
2026-06-24 17:42:29 +0530

    feat: add root endpoint to confirm API is running

--- src/server.js
+++ src/server.js
@@ -7,6 +7,10 @@ const routes = require("./routes");
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("CodeVector Products API Running");
+});
+
 app.use("/", routes);
 
 app.listen(process.env.PORT || 5000, () => {
